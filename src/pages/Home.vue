ed5905f18495cb34dceb18be79b1da45894f529f
SnekHunter <qawiem204@gmail.com>
2025-07-13 04:05:44 +0800

    combined About and Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -20,6 +20,28 @@
       This is much faster than traditional websites because we're not loading
       new pages from the server each time.
     </p>
+
+    <h1>About Me</h1>
+    <p>
+      I'm just another person trying to learn web development using React and
+      Vue. I'm excited about building websites and improve my current skills!
+    </p>
+
+    <h2>What I'm Learning:</h2>
+    <ul>
+      <li>HTML & CSS for structure and styling</li>
+      <li>JavaScript for making websites interactive</li>
+      <li>Vue.js for building dynamic applications</li>
+      <li>Vue Router for navigation (like this website!)</li>
+      <li>React for building user interfaces</li>
+    </ul>
+
+    <div class="highlight-box">
+      <p>
+        <strong>Fun fact:</strong> This entire website is actually just one HTML
+        file that shows different content using Vue Router!
+      </p>
+    </div>
   </div>
 </template>
 
